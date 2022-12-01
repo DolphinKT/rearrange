e67e2b671ab9362cb769ee37b9c3b47af92c6d4f
DolphinKT <119375879+DolphinKT@users.noreply.github.com>
2022-12-01 15:43:03 +0000

    Create rearrange.py

---
+++ rearrange.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import re
+
+def rearrange_name(name):
+    result = re.search(r"^([\w \.-]*), ([\w \.-]*)$",name)
+    if result is None:
+        return result
+    return "{} {}".format(result[2], result[1])
+    #fname= "{} {}".format(result[2], result[1])
+    #print(fname)
